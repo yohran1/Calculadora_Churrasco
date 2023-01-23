0d8872e89d37079c935d8ffbf9187c1b444b215c
Yohran Kallahari <yohranv@gmail.com>
2023-01-23 15:59:04 -0300

    Atualização no projeto

--- SCRIPT/churrascometro.js
+++ SCRIPT/churrascometro.js
@@ -1,47 +1,50 @@
-let inputAdultos = document.getElementById("adultos")
-let inputCriancas = document.getElementById("criancas")
-let inputDuracao = document.getElementById("duracao")
+// Carne - 400g por pessoa   + de 6 horas - 650
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
+// Crianças valem por 0,5 (Metade de cada item)
 
+let input_adultos = document.getElementsByTagName("input")[0]
+let input_criancas = document.getElementsByTagName("input")[1]
+let input_duracao = document.getElementsByTagName("input")[2]
+let button = document.getElementById("button")
 let resultado = document.getElementById("resultado")
 
-function calcular () {
+function calcular(){
+    console.log("Calculando...")
 
-    let adultos = inputAdultos.value;
-    let criancas = inputCriancas.value;
-    let duracao = inputDuracao.value;
+    let adultos = input_adultos.value
+    let criancas = input_criancas.value
+    let duracao = input_duracao.value
 
-    let quantidadeTotalCarne = carnePorPessoas(duracao) * adultos + (carnePorPessoas(duracao) / 2 * criancas)
-    let quantidadeTotalCerveja = cervejaPorPessoas(duracao) * adultos;
-    let quantidadeTotalBebidas = bebidasPorPessoas(duracao) * adultos + (bebidasPorPessoas(duracao) / 2 * criancas)
+    let quantidadeTotalCarne = carnePorPessoa(duracao) * adultos + (carnePorPessoa(duracao)/2 * criancas)
+    let quantidadeTotalCerveja = cervejaPorPessoa(duracao) * adultos
+    let quantidadeTotalBebidas = bebidasPorPessoa(duracao) * adultos + (bebidasPorPessoa(duracao)/2 * criancas)
 
-    resultado.innerHTML = `<p> ${quantidadeTotalCarne / 1000} kg de Carne</p>`
-    resultado.innerHTML += `<p> ${Math.ceil(quantidadeTotalCerveja / 355)} Latas de Cerveja</p>`
-    resultado.innerHTML += `<p> ${Math.ceil(quantidadeTotalBebidas / 2000)} Pet's 2l de Bebidas</p>`
+    resultado.innerHTML = `<p><b>${quantidadeTotalCarne /1000}kg de Carne</b></p>`
+    resultado.innerHTML += `<p><b>${Math.ceil(quantidadeTotalCerveja /355)} Latas de Cerveja</b></p>`
+    resultado.innerHTML += `<p><b>${Math.ceil(quantidadeTotalBebidas /2000)} Garrafas Pet's de Bebida</b></p>`
+
 }
-function carnePorPessoas(duracao) {
-    
-    if (duracao >= 6){
-        return 650;
-    }
-    else {
-        return 400;
-    }
-}
-function cervejaPorPessoas(duracao) {
-    
-    if (duracao >= 6){
-        return 2000;
-    }
-    else {
-        return 1200;
+function carnePorPessoa(duracao){
+    if(duracao >= 6){
+        return 650
+    }else{
+        return 400
     }
 }
-function bebidasPorPessoas(duracao) {
-    
-    if (duracao >= 6){
-        return 1500;
-    }
-    else {
-        return 1000;
+function cervejaPorPessoa(duracao){
+    if(duracao >= 6){
+        return 2000
+    }else{
+        return 1200
     }
 }
+function bebidasPorPessoa(duracao){
+    if(duracao >= 6){
+        return 1500
+    }else{
+        return 1000
+    }
+}
+
+button.addEventListener("click", calcular)
